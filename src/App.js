004c9463313ddcdd627d7dbd0f597e39780056b3
Ersin Aydın <ersinaydinworks@gmail.com>
2023-04-14 23:50:09 +0300

    add create task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import CreateTask from './CreateTask';
 import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CreateTask />
     </div>
   );
 }
---
+++ src/CreateTask.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function CreateTask() {
+    return (
+        <div className='todoDivArea'>
+            <form className='todoDiv'>
+                <input type="text" className='form-control todoText mb-3' name='title' placeholder='Başlık' />
+                <textarea name="content" className='form-control todoText mb-3' rows={3} placeholder='işinizi yazınız'></textarea>
+                <button className='btn btn-primary todoButton'>Ekle</button>
+            </form>
+        </div>
+    )
+}
