39048751e4cbd28b38f5f3cf9b0188740d9066a3
Ersin Aydın <ersinaydinworks@gmail.com>
2023-04-15 23:46:40 +0300

    detectChange test

--- src/CreateTask.jsx
+++ src/CreateTask.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function CreateTask() {
+    const [task, setTask] = useState({
+        title: '',
+        content: ''
+    })
+    function detectChange(event) {
+        console.log(event.target.value);
+    }
     return (
         <div className='todoDivArea'>
             <form className='todoDiv'>
-                <input type="text" className='form-control todoText mb-3' name='title' placeholder='Başlık' />
-                <textarea name="content" className='form-control todoText mb-3' rows={3} placeholder='işinizi yazınız'></textarea>
+                <input type="text" value={task.title} onChange={detectChange} className='form-control todoText mb-3' name='title' placeholder='Başlık' />
+                <textarea name="content" value={task.content} onChange={detectChange} className='form-control todoText mb-3' rows={3} placeholder='işinizi yazınız'></textarea>
                 <button className='btn btn-primary todoButton'>Ekle</button>
             </form>
         </div>
